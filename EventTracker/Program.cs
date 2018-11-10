fd986fafec91ba5a1488fa7e84c9e16f8c387d49
RHeegaard <rheegaard@outlook.com>
2018-11-10 16:00:17 +0100

    Added program

---
+++ EventTracker/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace EventTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sites = new List<string>();
+            using (WebClient client = new WebClient())
+            {
+                sites.Add(client.DownloadString("http://www.nintendo.dk/nyheder"));
+                sites.Add(client.DownloadString("http://www.nintendo.dk/pokemon"));
+                sites.Add(client.DownloadString("http://www.nintendo.no/nyheter"));
+                sites.Add(client.DownloadString("http://www.nintendo.no/pokemon"));
+                sites.Add(client.DownloadString("http://www.nintendo.se/nyheter"));
+                sites.Add(client.DownloadString("http://www.nintendo.se/pokemon"));
+                sites.Add(client.DownloadString("http://www.nintendo.fi/uutiset"));
+                sites.Add(client.DownloadString("http://www.nintendo.fi/pokemon"));
+            }
+            Console.WriteLine("Ready..");
+            bool close = false;
+            while (!close)
+            {
+                string pokemon = Console.ReadLine();
+                if (pokemon == "close") close = true;
+
+                string dkReg = "/nyheder/[a-z0-9/-]+pokemon[a-z0-9/-]+" + pokemon + "[a-z0-9/-]*";
+                string noReg = "/nyheter/[a-z0-9/-]+pokemon[a-z0-9/-]+" + pokemon + "[a-z0-9/-]*";
+                string seReg = "/nyheter/[a-z0-9/-]+pokemon[a-z0-9/-]+" + pokemon + "[a-z0-9/-]*";
+                string fiReg = "/uutiset/[a-z0-9/-]+pokemon[a-z0-9/-]+" + pokemon + "[a-z0-9/-]*";
+                Regex dkRegex = new Regex(dkReg);
+                Regex noRegex = new Regex(noReg);
+                Regex seRegex = new Regex(seReg);
+                Regex fiRegex = new Regex(fiReg);
+
+                var list = new List<string>();
+                foreach (Match match in dkRegex.Matches(sites[0]))
+                {
+                    string s = "http://www.nintendo.dk" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in dkRegex.Matches(sites[1]))
+                {
+                    string s = "http://www.nintendo.dk" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in noRegex.Matches(sites[2]))
+                {
+                    string s = "http://www.nintendo.no" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in noRegex.Matches(sites[3]))
+                {
+                    string s = "http://www.nintendo.no" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in seRegex.Matches(sites[4]))
+                {
+                    string s = "http://www.nintendo.se" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in seRegex.Matches(sites[5]))
+                {
+                    string s = "http://www.nintendo.se" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in fiRegex.Matches(sites[6]))
+                {
+                    string s = "http://www.nintendo.fi" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (Match match in fiRegex.Matches(sites[7]))
+                {
+                    string s = "http://www.nintendo.fi" + match.Value;
+                    if (!list.Contains(s))
+                    {
+                        list.Add(s);
+                    }
+                }
+                foreach (var s in list)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            
+        }
+    }
+}
